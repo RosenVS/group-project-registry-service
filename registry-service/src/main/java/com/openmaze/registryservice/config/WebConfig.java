8e676857cf42c5363608a0a66438aa9bf7190bc5
RosenVS <rosen200262@gmail.com>
2024-06-20 10:55:17 +0200

    da

--- registry-service/src/main/java/com/openmaze/registryservice/config/WebConfig.java
+++ registry-service/src/main/java/com/openmaze/registryservice/config/WebConfig.java
@@ -1,15 +1,25 @@
 package com.openmaze.registryservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@CrossOrigin(origins = "http://localhost:3000")
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/")
-                .allowedOrigins("*");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://groupproject-frontend-hlfxsphkja-ew.a.run.app", "http://localhost:3000");
+
+            }
+        };
     }
+
 }
